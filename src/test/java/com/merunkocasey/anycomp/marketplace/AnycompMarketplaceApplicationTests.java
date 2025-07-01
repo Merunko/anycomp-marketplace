7707f356d33cbf027ca1bd2bd8655dda7e47e88a
Merunko <caseyjordanp9@gmail.com>
2025-07-01 18:54:59 +0800

    Initial commit.

---
+++ src/test/java/com/merunkocasey/anycomp/marketplace/AnycompMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merunkocasey.anycomp.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnycompMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
